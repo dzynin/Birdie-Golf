812c37c6abaf9f0c5393b332ec849a04ce9390d7
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-07-28 18:06:02 -0600

    Coming back to finish 3D buttons

---
+++ Birdie Golf/SignInScreenView/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  Birdie Golf
+//
+//  Created by Isiah Parra on 7/28/22.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    @IBOutlet weak var buttonStackView: UIStackView!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
