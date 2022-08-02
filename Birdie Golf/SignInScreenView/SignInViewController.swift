9dc119fbd82007df89922c66ec9c4a04d165b55e
Isiah Parra <isiah.parra@gmail.com>
2022-08-02 11:13:46 -0600

    Deleted duplicate corner radius
    • updated signIn view controller and deleted duplicate corner radius

--- Birdie Golf/SignInScreenView/SignInViewController.swift
+++ Birdie Golf/SignInScreenView/SignInViewController.swift
@@ -31,15 +31,4 @@ class SignInViewController: UIViewController {
     */
 
 }
-@IBDesignable extension UIView {
-    @IBInspectable var cornerRadius: CGFloat {
-        get { return layer.cornerRadius }
-        set {
-              layer.cornerRadius = newValue
 
-              // If masksToBounds is true, subviews will be
-              // clipped to the rounded corners.
-              layer.masksToBounds = (newValue > 0)
-        }
-    }
-}
