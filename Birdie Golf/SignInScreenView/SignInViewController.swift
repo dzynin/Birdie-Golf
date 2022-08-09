139b5f36ded61d3d3e92b5fa26d3b4d09a4b9780
Isiah Parra <isiah.parra@gmail.com>
2022-08-09 12:42:43 -0600

    Updated versions on both storyboards
    Probably going to delete  “ Dont have an account?”  but everything else looks the way it should

--- Birdie Golf/SignInScreenView/SignInViewController.swift
+++ Birdie Golf/SignInScreenView/SignInViewController.swift
@@ -150,3 +150,23 @@ extension SignInViewController: ASAuthorizationControllerDelegate, ASAuthorizati
     
 }
 
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let cgColor = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+        set { layer.borderColor = newValue?.cgColor }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+}
